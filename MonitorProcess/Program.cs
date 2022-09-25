d6796a317c8a7ae9d0699995de7bd139bebb966f
João Agostinho <joao.agostinho900@hotmail.com>
2022-09-25 22:30:10 +0100

    add logic to worker

--- MonitorProcess/Program.cs
+++ MonitorProcess/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ORMi.Interfaces;
+using ORMi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- MonitorProcess/Worker.cs
+++ MonitorProcess/Worker.cs
@@ -1,29 +1,56 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ORMi;
+using ORMi.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Process = System.Diagnostics.Process;
 
 namespace MonitorProcess
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
-
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            Console.WriteLine("Insert process name:");
+            string processName = Console.ReadLine();
+
+            Console.WriteLine("Insert maximum lifetime in minutes:");
+            int maxLifeTime = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert monotoring frequency in minutes:");
+            int frequency = Int32.Parse(Console.ReadLine());
+
+            // Get Process 
+            Process process = Process.GetProcesses().ToList().FirstOrDefault(x => x.ProcessName.Contains(processName) == true);
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                // If the process isn't found
+                while(process == null)
+                {
+                    Console.WriteLine("There isn't a process with the name inputed. Insert process name:");
+                    processName = Console.ReadLine();
+                    process = Process.GetProcesses().ToList().FirstOrDefault(x => x.ProcessName.Contains(processName) == true);
+                }
+                
+                // If the process's start time has passed the lifetime
+                if(DateTime.Compare(process.StartTime, DateTime.Now.AddMinutes(maxLifeTime)) < 0)
+                {
+                    process.Kill();
+
+                    Console.WriteLine("The process has closed");
+                    await Task.Delay(5000, stoppingToken);
+                    System.Environment.Exit(0);
+                }
+
+                await Task.Delay(frequency / 60, stoppingToken);
             }
         }
+
     }
 }
